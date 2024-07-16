26ebb0339390b08ffae23a467f666731568f1718
harshl00 <63408264+harshl00@users.noreply.github.com>
2024-07-16 10:50:43 +0530

    Create f7.js

---
+++ f7.js
@@ -0,0 +1,7 @@
+public class UpdateWebsite {
+    public static void main(String[] args) {
+        String message = "WelcomePreshers";
+        String correctedMessage = message.replace("Preshers", " Freshers");
+        System.out.println(correctedMessage);
+    }
+}
