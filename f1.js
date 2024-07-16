0b6c6c475995f973875f669a1206562f922a01d9
harshl00 <63408264+harshl00@users.noreply.github.com>
2024-07-16 09:40:41 +0530

    Create f1.js

---
+++ f1.js
@@ -0,0 +1,6 @@
+5marks
+1)List and explain the control statements with suitable Java program.
+2)Mr Jone went to super market with a basket of holding 5kg he purchased 1 kg of apple,1kg of orange, 1kg of grapes, 1kg of pomegranate and 1kg of kiwi,he puts all the fruits in his basket and at home he wants to check what he has brought form the market.wirte the the Java code for the above scenario.
+3)imagine you have been tasked with designing a multi threaded application using Java, consider a situation where you need to pass parameters to the run method when creating and starting a thread how would you modify the code to achieve this.
+30 marks
+Define a interface area which should contain a method area()impliment this interface in rectangle and triangle classes to find there area.demonstrate the above implementation by writing the main method in a new class called demointerface area.
